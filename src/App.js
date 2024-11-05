33874a9d1d642fb486dec2ded1e34c26662f7b1c
Dakhani Usman <115381340+DakhaniUsman@users.noreply.github.com>
2024-11-05 21:21:05 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+      <h1>FSD - React Day 01</h1>
+      <h1>Dakhani Usman</h1>
+   </div>
+  );
+}
+
+export default App;
