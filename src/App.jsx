dd762c41197021c665741ad5518ffa499ebf4564
Dakhani Usman <115381340+DakhaniUsman@users.noreply.github.com>
2024-11-12 20:26:30 +0530

    product.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import NotFound from "./components/NotFound"
+import NotFound from "./components/NotFound";
+import Product from "./components/Product";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="*" element={<NotFound />}/>
+        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/product" element={<Product />} />
       </Routes>
     </div>
   );
---
+++ src/components/Product.jsx
@@ -0,0 +1,9 @@
+function Product() {
+  return (
+    <div>
+      <h1>Welcome to Product Page</h1>
+    </div>
+  );
+}
+
+export default Product;
